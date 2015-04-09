9f4263db93af7f58a6336c821987440fc77c1025
Joar Classon <joar@classon.se>
2015-04-09 23:08:17 +0200

    blargs

--- includes/pagehandler.php
+++ includes/pagehandler.php
@@ -6,4 +6,6 @@ $URL_DIVIDED = explode("/", $URL);
 if ($URL == "start" || $URL == "") {
   $PAGE = "start";
   $PAGE_INCLUDE = "start.php";
-} else if ($URL_DIVIDED[0] == "edit" && $URL_DIVIDED[1] == get)
+} else if ($URL_DIVIDED[0] == "edit" && $URL_DIVIDED[1] == "") {
+
+}
